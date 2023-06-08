8f1ade29ff2a73657ede3f29b965c75b9bda3c72
Michał Paczyński <michal.paka@op.pl>
2023-06-08 22:24:57 +0200

    username creation on sign in with provider

--- utils/auth/callbacks.ts
+++ utils/auth/callbacks.ts
@@ -1,25 +1,32 @@
 // Next imports
-import { Account, Profile, User } from 'next-auth';
+import { DefaultUser, User } from 'next-auth';
 import { AdapterUser } from 'next-auth/adapters';
 // Models imports
 import UserModel from '@/models/userModel';
 // Utils imports
 import { connectToDatabase } from '../database';
 
+const createUsername = (name: DefaultUser['name'], id: string): string => {
+  if (!name) return `user#${id}`;
+
+  const initials = name
+    .split(' ')
+    .map(el => el.slice(0, 1).toLowerCase())
+    .join('');
+
+  return `${initials}#${id}`;
+};
+
 // TODO: deal with sign in using different providers, but the same email
 export const signInCallback = async (user: User | AdapterUser): Promise<boolean> => {
   try {
     await connectToDatabase();
-
     const exisitingUser = await UserModel.findOne({ email: user.email });
 
     if (!exisitingUser) {
-      // TODO: develop better username creation
-      const username = `${user.name?.toLowerCase().replaceAll(' ', '.')}#${user.id}`;
-
       const newUserData = {
         email: user.email,
-        username,
+        username: createUsername(user.name, user.id),
         name: user.name,
         image: user.image,
       };
